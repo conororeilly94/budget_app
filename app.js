a91fdd4db501299f38de7718277e0f768f479a31
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-19 12:18:50 +0000

    read input data

--- app.js
+++ app.js
@@ -16,20 +16,44 @@ var budgetController = (function() {
 
 var UIController = (function() {
     
-    // Some code
+    var DOMstrings = {
+        // Storing all strings and retrieve and change if needed
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Reads value of the type. Either inc or exp v(+ or -)
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
     
 })();
 
 
 /////////////////////
-// Global App controller
+// Global App controller - Tell the other modules what to do
 /////////////////////
 
 var controller = (function(budgetCtrl, UICtrl) { // Can pass args into function
     
+    var DOM = UICtrl.getDOMstrings();
+    
     var ctrlAddItem = function() {
         
-         // 1. Get the field input data
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
         
         // 2. Add the item to the budget controller
         
@@ -38,10 +62,9 @@ var controller = (function(budgetCtrl, UICtrl) { // Can pass args into function
         // 4. Calculate the budget
         
         // 5. Display the budget on the UI
-        console.log('YES');
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     // Happens on the whole document, not a particular var
     // keypress - any button pressed
