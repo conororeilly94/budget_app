0427b2a600eb9e62ac99e47fe08a7169c6d1f3e5
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-19 11:50:30 +0000

    added initial event listeners

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
+////////////////////
+// Module to take care of the budget function
+////////////////////
+
 var budgetController = (function() {
     
-    var x = 23
-    
-    var add = function(a) {
-        return x + a; // Var and function cannot be accessed from the outside
-    }
-    
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }        
-    }
+    // Some code
     
 })(); // IIFE function - immediately invoked due to () on line 15
 // Inner functions can access vars and functions of outer functions due to CLOSURES even after outer function has returned
-
 
 
 ////////////////////
@@ -28,20 +21,37 @@ var UIController = (function() {
 })();
 
 
-
 /////////////////////
-// App controller
+// Global App controller
 /////////////////////
 
 var controller = (function(budgetCtrl, UICtrl) { // Can pass args into function
     
-    var z = budgetCtrl.publicTest(5);
-    
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+        
+         // 1. Get the field input data
+        
+        // 2. Add the item to the budget controller
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget on the UI
+        console.log('YES');
     }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    // Happens on the whole document, not a particular var
+    // keypress - any button pressed
+    document.addEventListener('keypress', function(event) {
+             
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+        
+    });
     
 })(budgetController, UIController);
 
