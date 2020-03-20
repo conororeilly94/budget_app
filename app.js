d0ac13a16b842eb657425c231cbc56695be5bb33
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-20 12:57:27 +0000

    adding month/year

--- app.js
+++ app.js
@@ -204,7 +204,8 @@ var UIController = (function () {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
     
     var formatNumber = function (num, type) {
@@ -322,6 +323,20 @@ var UIController = (function () {
                 }
             });
 
+        },
+        
+        displayMonth: function() {
+            var now, months, month, year;
+            
+            now = new Date();
+            // var christmas = new Date(2020, 11, 25);
+            
+            months = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+            
         },
 
         getDOMstrings: function () {
@@ -444,6 +459,7 @@ var controller = (function (budgetCtrl, UICtrl) { // Can pass args into function
     return {
         init: function () {
             console.log('App started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
